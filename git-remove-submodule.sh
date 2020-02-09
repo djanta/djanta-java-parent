484d8c378aa0af4bbc28d02cfed86b91360741ab
stanislaska <koffi.ass@gmail.com>
2020-02-09 15:25:32 -0600

    [WIP]: Refreshing travis ci submodule configuration

--- git-remove-submodule.sh
+++ git-remove-submodule.sh
@@ -13,26 +13,27 @@
 
 # Adapted from: https://gist.githubusercontent.com/sharplet/6289697/raw/2490096d3adb1139cd130faf70a69ed3537ce19e/git-remove-submodule
 
-submodule_name=$(echo "$1" | sed 's/\/$//'); shift
+path=$(echo "$1" | sed 's/\/$//'); shift
 
 exit_err() {
   [ $# -gt 0 ] && echo "fatal: $*" 1>&2
   exit 1
 }
 
-if git submodule status "$submodule_name" >/dev/null 2>&1; then
-  git submodule deinit -f "$submodule_name"
-  git rm --cached $submodule_name
-  rm -rf .git/modules/$submodule_name
-  git rm -f "$submodule_name"
-  git reset HEAD .gitmodules
+if git submodule status "$path" >/dev/null 2>&1; then
+  git submodule deinit -f "$path"
+  git rm --cached $path
+  rm -rf .git/modules/$path
+  git commit -m "Removed submodule $path"
+  git rm -f "$path"
+#  git reset HEAD .gitmodules
 
-  git config -f .gitmodules --remove-section "submodule.$submodule_name"
+  git config -f .gitmodules --remove-section "submodule.$path"
   if [ -z "$(cat .gitmodules)" ]; then
     git rm -f .gitmodules
   else
     git add .gitmodules
   fi
 else
-  exit_err "Submodule '$submodule_name' not found"
+  exit_err "Submodule '$path' not found"
 fi
